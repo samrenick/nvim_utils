db6443b154c66b05bf34cc145ea126f2f5010b55
Samuel Renick <samuelrenick@Samuels-MacBook-Air.local>
2021-03-31 19:15:54 -0400

    allow multiline snippets

--- tools.lua
+++ tools.lua
@@ -4,10 +4,15 @@ function _G.dump(...)
 end
 
 snippets = {
-    lpad = " '0') || lpad(stops.unique_id_no::text, 8, '0') AS stop_uid",
-    sprintf = "sprintf('%03s%08s', $co_num, $line['stop_uid'])" ,
-    show_errors = "ini_set('display_errors', '1'); ini_set('display_startup_errors', '1'); error_reporting(E_ALL);"
+    lpad = {" '0') || lpad(stops.unique_id_no::text, 8, '0') AS stop_uid"},
+    sprintf = {"sprintf('%03s%08s', $co_num, $line['stop_uid'])"} ,
+    show_errors = {
+        "ini_set('display_errors', '1');",
+        "ini_set('display_startup_errors', '1');",
+        "error_reporting(E_ALL);"
+    }
 }
+dump(#snippets.show_errors)
 
 function searchSnippets(input)
     for key, val in pairs(snippets) do
@@ -20,9 +25,17 @@ end
 function getSnippet()
     snip = searchSnippets(vim.api.nvim_eval('expand("<cword>")'))
     if snip ~= nil then
-        vim.api.nvim_set_current_line(snip)
+        setSnippet(snip)
     end
 end
+
+function setSnippet(snip)
+    currLine = vim.api.nvim_eval("line('.')")
+    length = #snip
+    -- vim.api.nvim_set_current_line(snip)
+    -- dump(snip)
+    vim.api.nvim_buf_set_lines(nil,currLine-1,currLine + length,false,snip)
+end 
 
 vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua getSnippet()<CR>', {noremap = true})
 
@@ -30,3 +43,4 @@ vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua getSnippet()<CR>', {noremap 
 --lpad
 --sprintf
 --show_errors
+-- dump()
